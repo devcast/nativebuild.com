c4e52ed24097ad082badb3b480e7f44f114a51a9
Felquis Gimenes <frgformenton@gmail.com>
2014-07-13 04:11:44 -0300

    Code refactor

--- _source/js/main.js
+++ _source/js/main.js
@@ -2,30 +2,31 @@
 
 ;(function () {
 
-	if (!!sessionStorage._nativebuild_header_sucks) {
-		return false;
-	}
+  if (!!sessionStorage._nativebuild_header_sucks) {
+    return false;
+  }
 
-	// Animação do nome do blog
-	var $h1 = document.querySelector('.header-entrada h1'),
-		text = $h1.innerText || $h1.textContent,
-		$key, i = 0;
+  // Animação do nome do blog
+  var $h1 = document.querySelector('.header-entrada h1'),
+      text = $h1.innerText || $h1.textContent,
+      $key, time,
+      i = 0;
 
-	$h1.removeChild($h1.firstChild);
-	sessionStorage._nativebuild_header_sucks = ':)';
+  $h1.removeChild($h1.firstChild);
+  sessionStorage._nativebuild_header_sucks = ':)';
 
-	var time = setInterval(function () {
-		$key = document.createElement('span');
-		$key.innerHTML = text[i];
+  time = setInterval(function () {
+    $key = document.createElement('span');
+    $key.innerHTML = text[i];
 
-		$h1.appendChild($key);
+    $h1.appendChild($key);
 
-		if (!!text[i + 1]) {
-			i++;
-		} else {
-			clearInterval(time);
-		}
-	}, 150);
+    if (!!text[i + 1]) {
+      i++;
+    } else {
+      clearInterval(time);
+    }
+  }, 150);
 }());
 
 (function () {
